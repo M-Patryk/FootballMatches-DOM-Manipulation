eb30852b9681b2555f23f4dadb65967f831cf8da
PatrykM <patrykxmajchrzak@gmail.com>
2020-10-28 19:24:32 +0100

    Added function to make list and another to print games

---
+++ app.js
@@ -0,0 +1,335 @@
+const games = [
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 5,
+			isWinner : true,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 0,
+			isWinner : false,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : true
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : true
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 3,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 2,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 2,
+			isWinner : false,
+			isDraw   : true
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 2,
+			isWinner : false,
+			isDraw   : true
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 2,
+			isWinner : true,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 2,
+			isWinner : true,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 3,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 4,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 3,
+			isWinner : true,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 2,
+			isWinner : true,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 0,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 4,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 2,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : true
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : true
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 2,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 3,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 0,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 3,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 2,
+			isWinner : false,
+			isDraw   : true
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 2,
+			isWinner : false,
+			isDraw   : true
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 5,
+			isWinner : true,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 0,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : true,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 0,
+			isWinner : false,
+			isDraw   : true
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 0,
+			isWinner : false,
+			isDraw   : true
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'Real Madrit',
+			scored   : 2,
+			isWinner : true,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'FC Barcelona',
+			scored   : 0,
+			isWinner : false,
+			isDraw   : false
+		}
+	},
+	{
+		homeTeam : {
+			name     : 'FC Barcelona',
+			scored   : 1,
+			isWinner : false,
+			isDraw   : false
+		},
+		awayTeam : {
+			name     : 'Real Madrit',
+			scored   : 3,
+			isWinner : true,
+			isDraw   : false
+		}
+	}
+];
+//Dla pogladu
+
+// {
+//     homeTeam: {
+//         name: 'FC Barcelona',
+//         scored: 1,
+//         isWinner: false,
+//         isDraw: false
+//     },
+//     awayTeam: {
+//         name: "Real Madrit",
+//         scored: 3,
+//         isWinner: true,
+//         isDraw: false
+//     }
+// }
+
+const makeList = (gamesList) => {
+	const parentUl = document.createElement('ul');
+	for (let game of gamesList) {
+		const li = document.createElement('li');
+        li.innerHTML = getInfo(game);
+        parentUl.append(li)
+    }
+    return parentUl
+};
+
+//function that gets info and prints it
+const getInfo = ({ homeTeam, awayTeam }) => {
+	const { name: nameHome, scored: scoreHome } = homeTeam;
+	const { name: nameAway, scored: scoreAway } = awayTeam;
+
+    const teamNames = `${nameHome} vs ${nameAway}`;
+    const teamScores = `${scoreHome} - ${scoreAway}`
+	return `${teamNames} ${teamScores}`;
+};
+const barcaSection = document.querySelector('#barca');
+const makeBarcaList = makeList(games);
+barcaSection.append(makeBarcaList)
